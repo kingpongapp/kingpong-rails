c401eaf0c07e2d6923a3ae1f88b0442c35a53d18
fouad <Chamma.fouad@gmail.com>
2018-02-07 10:27:42 +1100

    updated changes to player controller, routes, css

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,14 +4,24 @@ class PlayersController < ApplicationController
 		player.email = params[:email]
 		player.password = params[:password]
 		player.name = params[:name]
-		player.nickname = params[:nickname]
+		@nickname = params[:nickname]
 		player.image_url = params[:image]
 		player.save
-		redirect_to '/player/new'
+		render :welcome
 	end
 
-	def show
-		render :welcome
-	end	
 end
 
+	# def show
+		# player = Player.find_by(name: )
+		# @nickname = player.nickname
+		# render :welcome
+	# end
+	# require 'twitter'
+	
+	# client = Twitter::REST::Client.new do |config|
+		# config.consumer_key        = "oVJZUyR4pRgP5u78piwEzXTi8"
+		# config.consumer_secret     = "AOHeeWOY5wNfoq1y6VuzCUoPDbOREfk0UGRZHIjPx7hFTX4atV"
+		# config.access_token        = "960740313882009600-K9rIEKzC5O5sw3g1kH8PYZnjMeFFx48"
+		# config.access_token_secret = "oypKXUpp79RUgrH0MVtwXejfPQwqtXtCj7y9XxjrSkQbd"
+	# end
