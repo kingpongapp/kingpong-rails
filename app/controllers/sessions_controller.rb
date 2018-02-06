4f2bf3baf34a0176f13535f5b4ecfb9b07ad997d
roscolil <ross.lillis@gmail.com>
2018-02-06 15:41:45 +1100

    added sessions_helper file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,22 @@
 class SessionsController < ApplicationController
 
 	def home
-
 	end
 
+	def create
+    player = Player.find_by(email: params[:email])
+    if player && player.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      render :home
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+end
 
 end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,15 @@
+module SessionsHelper
+  
+  def current_user
+    Player.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    if current_user # or !! current_user
+      true
+    else
+      false
+    end
+  end
+
+end
