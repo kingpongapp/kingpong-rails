f16c796db32b257b44d79e805d58ab1cae708e6c
roscolil <ross.lillis@gmail.com>
2018-02-06 15:32:13 +1100

    logged in user setup

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,14 @@
 module PlayersHelper
+
+  def current_user
+    Player.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    if current_user # or !! current_user
+      true
+    else
+      false
+    end
+  end
 end
