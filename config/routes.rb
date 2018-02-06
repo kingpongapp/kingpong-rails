008d566117739c41bcc09e30cc09aa42278e03f5
roscolil <ross.lillis@gmail.com>
2018-02-06 12:20:53 +1100

    created home page login form and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :player_games
   resources :players
 
+  get '/', to: 'session#home'
+
   post '/session', to: 'session#create'
 
   delete '/session', to: 'session#destroy'
