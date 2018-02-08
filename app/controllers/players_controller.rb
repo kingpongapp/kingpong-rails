46bc8704e370a2e19c78ef4fd61e7687a867ae1e
fouad <Chamma.fouad@gmail.com>
2018-02-09 10:53:24 +1100

    updated database for profilepic and welcome page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -11,6 +11,12 @@ class PlayersController < ApplicationController
 		render :welcome
 	end
 
+	def update
+		playerpic = Player.find_by(id: session[:player_id])
+		@playerpic.profilepic = params[:profilepic]
+		playerpic
+	end	
+
 	def index
 		@players = Player.all
 	end
